6236063ecd56d977f063ab67d5ca6c728c1462ba
fbisceglia02 <ferruccio3214@gmail.com>
2023-09-28 06:31:01 +0200

    chore: logs

---
+++ .github/scripts/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "hello world"
+
+my_array=()
+
+my_array+=1
+my_array+=2
+my_array+=3
+my_array+=4
+
+for element in "{$my_array[@]}";
+do
+    echo "element is $element"
+done    
